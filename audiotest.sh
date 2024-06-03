e738cbe7ec1fdab3b064b2fc6d83690d30ae2f46
jonas.frey@unibe.ch <jonas.frey@unibe.ch>
2024-06-03 11:58:21 +0200

    asdf

--- audiotest.sh
+++
@@ -1,9 +0,0 @@
-curl https://api.openai.com/v1/audio/speech \
-  -H "Authorization: Bearer sk-proj-97TkvF5KzMtMLaPFLooVT3BlbkFJQ0WFgNyqxy95g3VlJiNH" \
-  -H "Content-Type: application/json" \
-  -d '{
-    "model": "tts-1",
-    "input": "The following is IPA and if spelled correctly it sounds like '/mʊmˈbaɪ/'",
-    "voice": "alloy"
-  }' \
-  --output test2.mp3
